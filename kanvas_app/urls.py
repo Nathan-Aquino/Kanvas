12175b43b71c90dbfdc6b205355458a2ca7050c7
Nathan <nathan.ar.94@gmail.com>
2021-08-04 23:57:26 -0300

    feat: added url to login view

--- kanvas_app/urls.py
+++ kanvas_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AccountsView
+from .views import AccountsView, LoginView
 
 urlpatterns = [
-    path('accounts/', AccountsView.as_view())
+    path('accounts/', AccountsView.as_view()),
+    path('login/', LoginView.as_view())
 ]
