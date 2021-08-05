877f7d8f254bc245f4f5c3f14ec7c96a68ed553e
Nathan <nathan.ar.94@gmail.com>
2021-08-05 17:55:58 -0300

    feat: Course model added

--- kanvas_app/models.py
+++ kanvas_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CharField
+from django.db.models.fields.related import ManyToManyField
 
-# Create your models here.
+class Course(models.Model):
+    name = CharField(max_length=255, unique=True)
+
+    users = ManyToManyField(User, related_name="courses")
+
+    def __str__(self):
+        return self.name
+
