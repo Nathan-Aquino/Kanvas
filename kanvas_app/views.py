b71a94e851b0b70965437f23889184fda2b743d4
Nathan <nathan.ar.94@gmail.com>
2021-08-05 00:11:13 -0300

    feat: added code for unauthorized errors

--- kanvas_app/views.py
+++ kanvas_app/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from kanvas_app.serializers import UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import serializers, status
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 
 class AccountsView(APIView):
@@ -22,4 +24,16 @@ class AccountsView(APIView):
         return Response(serializer.data)
 
 class LoginView(APIView):
-    ...
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(**serializer.validated_data)
+
+        if user:
+            token = Token.objects.get_or_create(user=user)[0]
+            return Response({'token': token.key})
+        else:
+            return Response({'Unauthorized': 'Failed to authenticate'}, status=status.HTTP_401_UNAUTHORIZED)
