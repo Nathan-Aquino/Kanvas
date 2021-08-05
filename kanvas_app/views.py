388003e0641b0dd01af2b0048fc2fb4450a320e4
Nathan <nathan.ar.94@gmail.com>
2021-08-04 22:59:38 -0300

    feat: added cadastration user view

--- kanvas_app/views.py
+++ kanvas_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from kanvas_app.serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
-# Create your views here.
+
+class AccountsView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        if User.objects.filter(username=serializer.validated_data['username']).exist():
+            return Response({'error':'user already exist!'},status=status.HTTP_409_CONFLICT)
