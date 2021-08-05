b63438d6b2ce9b6ed958756e0c96cafc6d2f9d34
Nathan <nathan.ar.94@gmail.com>
2021-08-04 23:56:37 -0300

    feat: code for user cadastration fixed

--- kanvas_app/serializers.py
+++ kanvas_app/serializers.py
@@ -4,5 +4,5 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
-    is_staff = serializers.BooleanField(required=False)
     is_superuser = serializers.BooleanField(required=False)
+    is_staff = serializers.BooleanField(required=False)
