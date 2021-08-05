8d02e14fe39b8410fc5a70555fb5b10b33e7c4c6
Nathan <nathan.ar.94@gmail.com>
2021-08-05 20:00:21 -0300

    feat: url to update enrolled students in the specific course

--- kanvas_app/urls.py
+++ kanvas_app/urls.py
@@ -4,5 +4,6 @@ from .views import AccountsView, CourseView, LoginView
 urlpatterns = [
     path('accounts/', AccountsView.as_view()),
     path('login/', LoginView.as_view()),
-    path('courses/', CourseView.as_view())
+    path('courses/', CourseView.as_view()),
+    path('courses/<int:course_id>/registrations/', CourseView.as_view())
 ]
