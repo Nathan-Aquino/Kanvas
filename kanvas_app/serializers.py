3ec913e3134a7981a37ea3e3a6929300a3fc8534
Nathan <nathan.ar.94@gmail.com>
2021-08-05 19:58:15 -0300

    feat: UserList and SimpleUser serializer added

--- kanvas_app/serializers.py
+++ kanvas_app/serializers.py
@@ -7,8 +7,14 @@ class UserSerializer(serializers.Serializer):
     is_superuser = serializers.BooleanField(required=False)
     is_staff = serializers.BooleanField(required=False)
 
+class SimpleUserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
 
 class CourseSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    users = UserSerializer(many=True, required=False)
+    users = SimpleUserSerializer(many=True, required=False)
+
+class UserListSerializer(serializers.Serializer):
+    user_ids = serializers.ListField()
