797d28b8caa8a432d1bd34c35ab0871252b086a5
Nathan <nathan.ar.94@gmail.com>
2021-08-07 17:30:01 -0300

    feat: url to delete and get a filtered list of courses included

--- kanvas_app/urls.py
+++ kanvas_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('accounts/', AccountsView.as_view()),
     path('login/', LoginView.as_view()),
     path('courses/', CourseView.as_view()),
+    path('courses/<int:course_id>/', CourseView.as_view()),
     path('courses/<int:course_id>/registrations/', CourseView.as_view())
 ]
