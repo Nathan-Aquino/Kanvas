38d3804e6fe8e4018797f50c3d016a56ff0c0422
Nathan <nathan.ar.94@gmail.com>
2021-08-08 23:05:59 -0300

    feat: url to submission and activity views added

--- kanvas_app/urls.py
+++ kanvas_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import AccountsView, CourseView, LoginView
+from .views import AccountsView, ActivitiesView, CourseView, LoginView, SubmissionsView
 
 urlpatterns = [
     path('accounts/', AccountsView.as_view()),
     path('login/', LoginView.as_view()),
     path('courses/', CourseView.as_view()),
     path('courses/<int:course_id>/', CourseView.as_view()),
-    path('courses/<int:course_id>/registrations/', CourseView.as_view())
+    path('courses/<int:course_id>/registrations/', CourseView.as_view()),
+    path('activities/', ActivitiesView.as_view()),
+    path('activities/<int:activity_id>/submissions/', SubmissionsView.as_view())
 ]
