d5c3f0995bee2636e65d9191dc0d453fed6dd5eb
Nathan <nathan.ar.94@gmail.com>
2021-08-08 23:06:54 -0300

    feat: permission for staff and superuser to create a activity added

--- kanvas_app/permissions.py
+++ kanvas_app/permissions.py
@@ -4,3 +4,7 @@ import ipdb
 class OnlyInstructor(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user.is_superuser or request.method in SAFE_METHODS)
+
+class InstructorAndStaffs(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_superuser or request.user.is_staff)
